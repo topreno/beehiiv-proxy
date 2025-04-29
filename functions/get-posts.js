f22fae90772b6806511c86844ac80ca8278bd510
jared chadwick <jaredchadwick@jareds-air.home>
2025-04-29 16:16:11 -0400

    Fix: added pub_ prefix to publication ID

--- functions/get-posts.js
+++ functions/get-posts.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  const response = await fetch('https://api.beehiiv.com/v2/publications/49a33715-255e-47d6-9a81-eb1b79f60f36/posts', {
+  const response = await fetch('https://api.beehiiv.com/v2/publications/pub_49a33715-255e-47d6-9a81-eb1b79f60f36/posts', {
     headers: {
       'Authorization': 'Bearer L49zwlDUjwSqF4PWIIPkCxUauGqtbkD3d2SKJz6XSoQCh6PjEOKo9mPQEowylak3'
     }
